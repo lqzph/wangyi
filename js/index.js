28e0486faef9567c0480304d8694785456af3a55
lqzph <654980002@qq.com>
2016-12-01 23:45:10 +0800

    wangyi

---
+++ js/index.js
@@ -0,0 +1,16 @@
+   $("#box").fullpage({
+      verticalCentered:false,
+       css3:true,
+       sectionsColor:['#254875', '#00FF00', '#254587', '#695684', '#00FF00', '#254587', '#695684', '#00FF00', '#254587', '#695684'],
+      anchors:['1', '2', '3', '4', '5', '6', '7', '8', '9', 'page10'],
+      navigation:true,
+      navigationPosition:"right",
+      navigationTooltips:["首页","视觉","交互","皮肤","功能","待办邮件","联系人邮件","科技","连接易信","马上体验"]
+   });
+   
+
+
+
+
+
+
